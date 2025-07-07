4c661ee97ac7d62130803ad1e8249655d85d200e
Aleksand Tselik <a.celik@z-it.ru>
2025-07-07 21:52:59 +0300

    added constructor for offer

--- BondSharp.OpenApi.Core/Data/Offer.cs
+++ BondSharp.OpenApi.Core/Data/Offer.cs
@@ -6,14 +6,20 @@ public struct Offer : IComparable<Offer>, IEquatable<Offer>
     public double Price { get; set; }
     public int Volume { get; set; }
 
+    public Offer(double price, int volume)
+    {
+        Price = price;
+        Volume = volume;
+    }
+
     public int CompareTo(Offer other)
     {
-       return Price.CompareTo(other.Price);
+        return Price.CompareTo(other.Price);
     }
 
     public bool Equals(Offer other)
     {
-       return  this.Price == other.Price && this.Volume == other.Volume;
+        return this.Price == other.Price && this.Volume == other.Volume;
     }
 
     public override bool Equals([NotNullWhen(true)] object? obj)
